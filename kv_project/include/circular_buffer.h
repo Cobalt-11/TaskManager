a9ee1c5fed6dda4eaaf94a0b3a34caef46a4635f
pracny <zephyrusg@proton.me>
2024-12-16 12:54:47 +0100

    update to header file

---
+++ kv_project/include/circular_buffer.h
@@ -0,0 +1,54 @@
+//header guard
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+
+#define MAX_BUFFER_SIZE 20
+#define MAX_TITLE_LENGTH 50
+
+typedef enum {
+	LOW 	= 1,
+	MEDIUM 	= 2,
+	HIGH	= 3 
+	 } priorityLevel;
+
+typedef enum {
+	ACTIVE	 = 1,
+	FINISHED = 2
+	 } taskStatus;
+
+typedef struct  {
+	char title[MAX_TITLE_LENGTH];
+	priorityLevel priority;
+	short day;
+	short month;
+	taskStatus status;
+	} taskStruct;
+
+typedef struct{
+
+	taskStruct tStruct[MAX_BUFFER_SIZE];
+	int elementCounter;
+	void *head; //pointer to head
+	void *tail; //pointer to tail
+	} circular_buffer;
+
+//Inicijalizira strukturu
+void initialize(circular_buffer *cb);
+
+//Dodaje element u strukturu
+void addElement(circular_buffer *cb, taskStruct *ts);
+
+//Mice element iz strukture
+void removeElement(circular_buffer *cb, int isRemoved);
+
+//Provjerava da li je struktura prazna
+int isEmpty(circular_buffer *cb);
+
+//Provjerava da li je struktura puna
+int isFull(circular_buffer *cb);
+
+//Ispisuje buffer tj.strukturu
+void printBuffer(circular_buffer *cb);
+
+#endif
+//End header guard
