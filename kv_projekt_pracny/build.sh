f0e8bc4d4001212cf2f6f4b5b17a49d590fa502b
Cobalt11 <55240614+Cobalt-11@users.noreply.github.com>
2025-01-29 01:00:13 +0100

    29.1.

--- kv_projekt_pracny/build.sh
+++ kv_projekt_pracny/build.sh
@@ -6,7 +6,7 @@ gcc -O3 -Wall -I./include -c ./src/task_manager.c -o ./obj/task_manager.o
 gcc -O3 -Wall -I./include -c ./src/main.c -o ./obj/main.o
 
 # Create static library
-ar rcs ./lib/libkv_project.a ./obj/circular_buffer.o ./obj/task_manager.o ./obj/main.o
+ar rcs ./lib/lib_project.a ./obj/circular_buffer.o ./obj/task_manager.o ./obj/main.o
 
 # Link object files and static library to create executable
 gcc -O3 -Wall -I./include -o ./bin/Run ./obj/main.o ./lib/libkv_project.a
